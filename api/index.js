356c1272b1ed787c8d7bd8568aaba6f740f48573
wzulfikar <me@wzulfikar.com>
2020-11-04 17:01:34 +0200

    chore: allow CORS

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const requestIp = require("request-ip");
 const geoip = require("geoip-lite");
 
-module.exports = (req, res) => {
+module.exports = cors((req, res) => {
+  // Get client IP and do the lookup
   const ip = requestIp.getClientIp(req);
   const geo = geoip.lookup(ip);
 
   res.send({ ip, geo });
-};
+});
