42b90b6256ecee85506ab450bc77f4214312f990
wzulfikar <me@wzulfikar.com>
2020-11-04 17:15:49 +0200

    fix: remove undefined variable

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const requestIp = require("request-ip");
 const geoip = require("geoip-lite");
 
-module.exports = cors((req, res) => {
+module.exports = (req, res) => {
   // Get client IP and do the lookup
   const ip = requestIp.getClientIp(req);
   const geo = geoip.lookup(ip);
 
   res.send({ ip, geo });
-});
+};
